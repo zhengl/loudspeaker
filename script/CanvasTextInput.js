a8f089dd76646b180599b1333acd847433167e4e
Zheng Li <loylizh@gmail.com>
2013-07-19 17:41:47 +0800

    Add blinking Cursor

--- script/CanvasTextInput.js
+++ script/CanvasTextInput.js
@@ -1,6 +1,7 @@
 function CanvasTextInput(context){
 	this.context = context;
 	this.createDOMElement();
+	this.createCursor();
     this.text = new Text("");
 }
 
@@ -10,15 +11,51 @@ CanvasTextInput.prototype.constructor = CanvasTextInput;
 CanvasTextInput.prototype.createDOMElement = function() {
 	this.element = document.createElement("input");
     this.element.type = "text";
+    this.element.style.background = "transparent";
+    this.element.style.border = "none";
+    // this.element.style.fontSize = "30px";
+    // this.element.style.fontFamily = "Calibri";
+    // this.element.style.margin = "0px 0px 0px 0px";
+    // this.element.style.padding = "0px 0px 0px 0px";
+    this.element.style.left = "-1000px";
+	this.element.style.top = "0px";
     this.context.stage.getContainer().appendChild(this.element);
 
     this.element.style.position = "absolute";
     this.element.focus();
 };
 
+CanvasTextInput.prototype.createCursor = function() {
+	this.cursor = new Kinetic.Line({
+        points: [0, 0, 0, 30],
+        stroke: 'black',
+        strokeWidth: 2,
+    });
+    this.context.layer.add(this.cursor);
+    this.context.layer.draw();
+
+    var self = this;
+    var total = 0;
+    var period = 500;
+    var anim = new Kinetic.Animation(function(frame) {
+    	if (frame.time > period + total) {
+        	self.cursor.setOpacity(self.cursor.getOpacity() == 1 ? '0' : '1');
+    		total += period;
+    	}
+    }, this.context.layer);
+
+    anim.start();
+};
+
+CanvasTextInput.prototype.adjustCursor = function() {
+	var textPosition = this.getText().getPosition();
+	var textWidth = this.context.getLastDraftItem().getKineticShape().getWidth();
+	this.cursor.setPosition(textPosition.x + textWidth + 2, textPosition.y);
+};
+
 CanvasTextInput.prototype.enableEventHandling = function(text) {
 	var self = this;
-    this.element.onkeydown = function(event){
+    this.element.onkeyup = function(event){
     	if(event.keyCode == 13) { // when press Enter
 	        self.context.getEventBus().publish(
 				new AbstractEvent(Page.Event.FINISH_TEXTING)
@@ -26,6 +63,7 @@ CanvasTextInput.prototype.enableEventHandling = function(text) {
 	    } else {
 	    	self.text.setValue(self.element.value);
 	    	self.write(self.text);
+	    	self.adjustCursor();
 	    }
     };
 };
@@ -54,7 +92,6 @@ CanvasTextInput.prototype.flush = function() {
 	this.text.setValue(this.element.value);
 	this.context.clearDraftItems();
 	var item = this.context.write(this.getText());
-	// console.log(this.context);
 	if (undefined != this.context.getEventBus()) {
 		item.registerEventBus(this.context.getEventBus());
 	}
@@ -70,8 +107,7 @@ CanvasTextInput.prototype.getText = function() {
 CanvasTextInput.prototype.setPosition = function(point){
 	this.position = point;
 	this.getText().setPosition(point);
-	this.element.style.left = point.x + "px";
-	this.element.style.top = point.y + "px";
+	this.cursor.setPosition(point.x, point.y);
 };
 
 CanvasTextInput.prototype.getPosition = function(){
