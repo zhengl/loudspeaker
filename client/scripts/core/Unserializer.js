48339168d4981b7b20ee1ab71db27cd9f6027213
Zheng Li <loylizh@gmail.com>
2013-09-02 16:14:26 +0800

    Revert "Refactor Page, unserialize context"
    This reverts commit 776d791204fdf84f2761340d831a08054d9c4fe3.

--- client/scripts/core/Unserializer.js
+++ client/scripts/core/Unserializer.js
@@ -1,28 +1,28 @@
-define('Unserializer', ['Line', 'Text'], function(Line, Text){
+define('UnserializeStrategy', ['Line', 'Text'], function(Line, Text){
 
 
-function Unserializer(){
+function UnserializeStrategy(){
 	
 }
 
-Unserializer.prototype.process = function(context, json) {
-	context.setUUID(json.uuid);
+UnserializeStrategy.prototype.process = function(page, json) {
+	page.setUUID(json.uuid);
 	var items = json.items;
 	for(var i = 0; i < items.length; i++){
 		switch(items[i].type) {
 			case 'line':
 				var line = Line.unserialize(items[i]);
-				context.draw(line);
+				page.draw(line);
 			break;
 			case 'text':
 				var text = Text.unserialize(items[i]);
-				context.write(text);
+				page.write(text);
 			break;
 		}
 	}
 };
 
-return Unserializer;
+return UnserializeStrategy;
 
 
 });
