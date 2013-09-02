776d791204fdf84f2761340d831a08054d9c4fe3
Zheng Li <loylizh@gmail.com>
2013-09-02 16:11:34 +0800

    Refactor Page, unserialize context

--- client/scripts/core/Unserializer.js
+++ client/scripts/core/Unserializer.js
@@ -1,28 +1,28 @@
-define('UnserializeStrategy', ['Line', 'Text'], function(Line, Text){
+define('Unserializer', ['Line', 'Text'], function(Line, Text){
 
 
-function UnserializeStrategy(){
+function Unserializer(){
 	
 }
 
-UnserializeStrategy.prototype.process = function(page, json) {
-	page.setUUID(json.uuid);
+Unserializer.prototype.process = function(context, json) {
+	context.setUUID(json.uuid);
 	var items = json.items;
 	for(var i = 0; i < items.length; i++){
 		switch(items[i].type) {
 			case 'line':
 				var line = Line.unserialize(items[i]);
-				page.draw(line);
+				context.draw(line);
 			break;
 			case 'text':
 				var text = Text.unserialize(items[i]);
-				page.write(text);
+				context.write(text);
 			break;
 		}
 	}
 };
 
-return UnserializeStrategy;
+return Unserializer;
 
 
 });
---
+++ client/spec/UnserializerSpec.js
@@ -0,0 +1,54 @@
+require(['Unserializer', 'Context', 'Line', 'Text'], function(Unserializer, Context, Line, Text, Point){
+
+
+describe("Unserializer", function(){
+		var context;
+
+		beforeEach(function(){
+			context = new Context();
+		});
+
+		it("unserialize", function(){
+			var unserializer = new Unserializer();
+			unserializer.process(context, {
+				uuid: "00000000-0000-0000-0001-000000000000",
+				items: [
+					{
+						uuid: "00000000-0000-0000-0000-000000000001",
+						type: "line",
+						color: "black",
+						points: [{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2}],
+						position: {x: 10, y: 10},
+					},
+					{
+						uuid: "00000000-0000-0000-0000-000000000002",
+						type: "text",
+						color: "blue",
+						content: "Hello World!",
+						position: {x: 20, y: 20},
+					},
+					]
+			});
+
+			var items = context.getItems();
+			expect(context.getUUID()).toEqual("00000000-0000-0000-0001-000000000000");
+			expect(items.length).toEqual(2);
+			
+			var line = items[0];
+			expect(line instanceof Line).toBe(true);
+			expect(line.getUUID()).toEqual("00000000-0000-0000-0000-000000000001");
+			expect(line.getColor()).toEqual('black');
+			expect(line.getPosition()).toEqual({x: 10, y: 10});
+			expect(line.getPoints()).toEqual([{x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2}]);
+
+			var text = items[1];
+			expect(text instanceof Text).toBe(true);
+			expect(text.getUUID()).toEqual("00000000-0000-0000-0000-000000000002");
+			expect(text.getValue()).toEqual('Hello World!');
+			expect(text.getColor()).toEqual('blue');
+			expect(text.getPosition()).toEqual({x: 20, y: 20});
+		});
+});
+
+
+});
